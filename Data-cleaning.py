0348cd488a48e5a446f5441d3011f9a77aad6aa1
Walter <waltercastilo6@hotmail.com>
2021-12-15 20:51:40 -0500

    Second add

--- Data-cleaning.py
+++ Data-cleaning.py
@@ -2,8 +2,36 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
  
-ad_location = pd.read_excel('Data/DMDC_Website_Location_Report_2109.xlsx')
-ad_rank = pd.read_excel('Data/AD_by_rank_2021.xlsx')
-ad_location = ad_location.loc[:, ['ARMY']]
-print(ad_location.head(10))
+ad_marital_status = pd.read_excel ('Data/AD-by-marital-status.xls')
+enlisted_df = ad_marital_status[:9]
+enlisted_df2 = enlisted_df.drop(columns=['Single Total', 'Joint Service Marriage Total', 'Single Parent Total', 'Civilian Married Total', 'Grand Total','Total Male', 'Total Female'])
+enlisted_df3 = enlisted_df.drop(columns=['Single Male', 'Single Female', 'Joint Service Marriage Male', 'Joint Service Marriage Female','Single Parent Male','Single Parent Female', 'Civilian Married Male','Civilian Married Female', 'Grand Total','Total Male', 'Total Female'])
+x = enlisted_df3['Pay Grade']
+y = enlisted_df3['Single Total']
+fig, ax = plt.subplots(figsize=(16,8)) 
+ax.plot(x, enlisted_df3['Single Total'], label = 'Single Total') 
+ax.plot(x, enlisted_df3['Single Parent Total'],label = 'Single Parent Total')
+ax.plot(x, enlisted_df3['Joint Service Marriage Total'],label = 'Joint Service Marriage Total')
+ax.plot(x, enlisted_df3['Civilian Married Total'],label = 'Civilian Marriage Total')
+plt.legend(loc="upper right")
+plt.title('Enlisted Service Members by Marital Status')
+plt.xlabel('Enlisted Pay Grade')
+plt.ylabel('Number of Service Members')
+plt.rcParams.update({'font.size': 18})
+plt.grid()
+plt.show()
 
+officer_df = ad_marital_status[10:20]
+officer_df3 = officer_df.drop(columns=['Single Male', 'Single Female', 'Joint Service Marriage Male', 'Joint Service Marriage Female','Single Parent Male','Single Parent Female', 'Civilian Married Male','Civilian Married Female', 'Grand Total','Total Male', 'Total Female'])
+x = officer_df3['Pay Grade']
+fig, ax = plt.subplots(figsize=(16,8)) 
+ax.plot(x, officer_df3['Single Total'], label = 'Single Total') 
+ax.plot(x, officer_df3['Single Parent Total'],label = 'Single Parent Total')
+ax.plot(x, officer_df3['Joint Service Marriage Total'],label = 'Joint Service Marriage Total')
+ax.plot(x, officer_df3['Civilian Married Total'],label = 'Civilian Marriage Total')
+plt.legend(loc="upper right")
+plt.title('Officer Service Members by Marital Status')
+plt.xlabel('Officer Pay Grade')
+plt.ylabel('Number of Service Members')
+plt.rcParams.update({'font.size': 18})
+plt.grid()
