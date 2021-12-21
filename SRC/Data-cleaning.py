2154d59720d20e31af88dfc0ff86bbb2ea259dd3
walter <waltercastilo6@gmail.com>
2021-12-21 15:18:04 -0500

    bar chart

--- SRC/Data-cleaning.py
+++ SRC/Data-cleaning.py
@@ -1,3 +1,4 @@
+#Imports
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,7 +13,7 @@ import scipy as sp
 import scipy.stats as stats
 from statsmodels.stats.proportion import proportions_ztest
 
-#import excel and create dataframes for officers and enlisted
+#Read excel and create dataframes for officers and enlisted
 ad_marital_status = pd.read_excel ('../Data/AD-by-marital-status.xls')
 ad_marital_index = ad_marital_status.set_index('Pay Grade')
 
@@ -48,7 +49,8 @@ def marital_status_plot_totals(df, enlisted=False):
 
 
 def marital_status_bar_totals(df, enlisted=True):
-
+    """Bar plot that displays the relationship between rank and marital status 
+    takes in a dataframe and whether it contains enlisted or officers"""
     single_total= df['Single Total'].values
     single_parent_total= df['Single Parent Total'].values
     joint_service_marriage_total = df['Joint Service Marriage Total'].values
@@ -79,6 +81,8 @@ def marital_status_bar_totals(df, enlisted=True):
     return plt.show()
 
 def marital_status_bar_mean(df ):
+    '''Function that creates bar plot of mean of officer or enlisted married personel by rank
+    takes in a dataframe'''
 
     single_total = df['Single Total'].values
     single_parent_total = df['Single Parent Total'].values
@@ -89,7 +93,7 @@ def marital_status_bar_mean(df ):
     single_parent_total_mean =np.mean(single_parent_total)
     joint_service_marriage_total_mean = np.mean(joint_service_marriage_total)
     civilian_married_total_mean = np.mean(civilian_married_total)
-    y = single_parent_total_mean, single_parent_total_mean, joint_service_marriage_total_mean, civilian_married_total_mean
+    y = single_total_mean, single_parent_total_mean, joint_service_marriage_total_mean, civilian_married_total_mean
     labels = ['Single', 'Single Parent', 'Joint Service Marriage', 'Civilian Marriage' ]
 
     x=np.arange(len(labels))
@@ -98,8 +102,6 @@ def marital_status_bar_mean(df ):
     fig, ax = plt.subplots(figsize=(20,8)) 
     plt.bar(x, y,color=['red', 'blue', 'green', 'black'] )
     
-
-   
     plt.xticks(x, labels)
   
     plt.title('Service Members by Marital Status')
@@ -108,36 +110,81 @@ def marital_status_bar_mean(df ):
     plt.rcParams.update({'font.size': 18})
     return plt.show()
 
+def ad_marital_status_bar_mean():
+    '''Function that creates bar plot of mean of officer or enlisted married personel by rank
+    takes in a dataframe'''
+    single_total = int(ad_marital_index.loc[['TOTAL ENLISTED'],'Single Total'].values) + int(ad_marital_index.loc[['TOTAL OFFICER'],'Single Total'].values)
+    single_parent_total = int(ad_marital_index.loc[['TOTAL ENLISTED'],'Single Parent Total'].values) + int(ad_marital_index.loc[['TOTAL OFFICER'],'Single Parent Total'].values)
+    joint_service_marriage_total = int(ad_marital_index.loc[['TOTAL ENLISTED'],'Joint Service Marriage Total'].values) + int(ad_marital_index.loc[['TOTAL OFFICER'],'Joint Service Marriage Total'].values)
+    civilian_married_total = int(ad_marital_index.loc[['TOTAL OFFICER'],'Civilian Married Total'].values) + int(ad_marital_index.loc[['TOTAL ENLISTED'],'Civilian Married Total'].values)
+
+
+    single_total_mean =np.mean(single_total)
+    single_parent_total_mean =np.mean(single_parent_total)
+    joint_service_marriage_total_mean = np.mean(joint_service_marriage_total)
+    civilian_married_total_mean = np.mean(civilian_married_total)
+    y = single_total_mean, single_parent_total_mean, joint_service_marriage_total_mean, civilian_married_total_mean
+    labels = ['Single', 'Single Parent', 'Joint Service Marriage', 'Civilian Marriage' ]
+
+    x=np.arange(len(labels))
+    width = 0.23
+
+    fig, ax = plt.subplots(figsize=(20,8)) 
+    plt.bar(x, y,color=['red', 'blue', 'green', 'black'] )
+    
+    plt.xticks(x, labels)
+  
+    plt.title('Service Members by Marital Status')
+    plt.xlabel('Marital Status')
+    plt.ylabel('Average Number of Service Members')
+    plt.rcParams.update({'font.size': 18})
+    return plt.show()
 
 def ztest_variables():
 
-    global married_prop_e, married_prop_o, total_e, total_o
+    global married_prop_e, married_prop_o, married_prop, unmarried_prop_e, unmarried_prop_o, unmarried_prop, total_o, total_sm, total_e, total_unmarried, total_married
     ad_marital_index = ad_marital_status.set_index('Pay Grade')
 
+    single_e = int(ad_marital_index.loc[['TOTAL ENLISTED'],'Single Total'].values)
+    single_parent_e = int(ad_marital_index.loc[['TOTAL ENLISTED'],'Single Parent Total'].values)
     js_married_e = int(ad_marital_index.loc[['TOTAL ENLISTED'],'Joint Service Marriage Total'].values)
     civ_married_e = int(ad_marital_index.loc[['TOTAL ENLISTED'],'Civilian Married Total'].values)
-
     total_e = int(ad_marital_index.loc[['TOTAL ENLISTED'],'Grand Total'].values)
     enlisted_married = int(js_married_e) + int(civ_married_e)
-    married_prop_e = enlisted_married/total_e
+    enlisted_unmarried = single_parent_e + single_e
 
-    ad_marital_index = ad_marital_status.set_index('Pay Grade')
+
+    single_o = int(ad_marital_index.loc[['TOTAL OFFICER'],'Single Total'].values)
+    single_parent_o = int(ad_marital_index.loc[['TOTAL OFFICER'],'Single Parent Total'].values)
     js_married_o = int(ad_marital_index.loc[['TOTAL OFFICER'],'Joint Service Marriage Total'].values)
     civ_married_o = int(ad_marital_index.loc[['TOTAL OFFICER'],'Civilian Married Total'].values)
-
     total_o = int(ad_marital_index.loc[['TOTAL OFFICER'],'Grand Total'].values)
     officer_married = int(js_married_o) + int(civ_married_o)
+    officer_unmarried = single_parent_o + single_o
+
+
+    total_e = int(ad_marital_index.loc[['TOTAL ENLISTED'],'Grand Total'].values)
+    total_o = int(ad_marital_index.loc[['TOTAL OFFICER'],'Grand Total'].values)
+    total_sm =total_e + total_o
+    total_married = enlisted_married + officer_married
+    total_unmarried = enlisted_unmarried + officer_unmarried
+    unmarried_prop_e = enlisted_unmarried/total_e
+    unmarried_prop_o = officer_unmarried/total_o
+    unmarried_prop = total_unmarried / total_sm
     married_prop_o = officer_married/total_o
+    married_prop_e = enlisted_married/total_e
+    married_prop = total_married / total_sm
 
-def test():
+
+def test(proportion_A, proportion_B, population_A, population_B):
     ztest_variables()
-    significance = 0.025
+    significance = 0.05
 
-    sample_prop_e, sample_size_e = (married_prop_e, total_e)
-    sample_prop_o, sample_size_o = (married_prop_o, total_o)
+    sample_prop_A, sample_size_A = (proportion_A, population_A)
+    sample_prop_B, sample_size_B = (proportion_B, population_B)
 
-    successes = np.array([sample_prop_e, sample_prop_o])
-    samples = np.array([sample_size_e, sample_size_o])
+    successes = np.array([sample_prop_A, sample_prop_B])
+    samples = np.array([sample_size_A, sample_size_B])
 
 
     stat, p_value = proportions_ztest(count=successes, nobs=samples,  alternative='two-sided')
@@ -160,4 +207,4 @@ if __name__ == '__main__':
     
     #marital_status_bar_mean(officer_totals)
     
-    test()
+    ztest_variables()
